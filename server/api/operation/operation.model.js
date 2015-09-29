59ae25ddce1ff0fac2304165e1abb40f502d367a
Albert <kovalevskij.albert@gmail.com>
2015-09-29 10:24:40 +0300

    operation model

--- server/api/operation/operation.model.js
+++ server/api/operation/operation.model.js
@@ -4,9 +4,27 @@ var dynamoose = require('dynamoose'),
     Schema = dynamoose.Schema;
 
 var OperationSchema = new Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  id: {
+    type: Number
+  },
+  sumTotal: {
+    type: Number
+  },
+  currency: {
+    type: Object
+  },
+  remindDuration: {
+    type: Object
+  },
+  initiator: {
+    type: Object
+  },
+  executor: {
+    type: Object
+  },
+  state: {
+    type: String
+  }
 });
 
 module.exports = dynamoose.model('Operation', OperationSchema);
