d1beb1977c855740384ec9d40800acb7054dc9ac
Albert <kovalevskij.albert@gmail.com>
2015-10-02 09:30:09 +0300

    refactor auth, if no token provided return 401

--- server/auth/index.js
+++ server/auth/index.js
@@ -11,6 +11,32 @@ module.exports = function () {
   return function (req, res, next) {
     var token = req.body.token || req.query.token || req.headers['x-access-token'] || req.headers['authorization'];
 
+    function requestAuthService(options) {
+      request(options, function (err, response, body) {
+        if (err) {
+          return res.json({success: false, message: 'Failed to authenticate'});
+        }
+        if (!err && response.statusCode === 200) {
+          console.log('Successful authorization');
+
+          //save authorized account in memory
+          var account = {
+            token: token,
+            body: JSON.parse(body)
+          };
+          req.authId = account.body.account.authId;
+          req.account = account;
+          inMemoryAccounts.push(account);
+          next();
+        } else {
+          res.status(response.statusCode).send({
+            success: false,
+            message: 'Could not get response.'
+          });
+        }
+      });
+    }
+
     if (token) {
       var inMemoryAccount = _.find(inMemoryAccounts, function (item) {
         return item.token === token;
@@ -27,44 +53,28 @@ module.exports = function () {
       if (inMemoryAccount && !(tokenExpiresIn < 1)) {
         console.log('Already authorized');
         req.authId = inMemoryAccount.body.account.authId;
+        req.account = inMemoryAccount;
         next();
       }
       else if (!inMemoryAccount || tokenExpiresIn < 1) {
-
         var options = {
           url: config.auth,
           headers: {
             'Authorization': token
           }
         };
-        request(options, function (err, response, body) {
-          if (err) {
-            return res.json({success: false, message: 'Failed to authenticate'});
-          }
-          if (!err && response.statusCode === 200) {
-            console.log('Successful authorization');
-
-            //save authorized account in memory
-            var account = {
-              token: token,
-              body: JSON.parse(body)
-            };
-            req.authId = account.body.account.authId;
-            inMemoryAccounts.push(account);
-            next();
-          } else {
-            res.status(response.statusCode).send({
-              success: false,
-              message: 'Could not get response.'
-            });
-          }
-        });
+        requestAuthService(options);
       } else {
-        return res.status(403).send({
+        return res.status(401).send({
           success: false,
-          message: 'No token provided.'
+          message: 'Unauthorized!.'
         });
       }
+    } else {
+      return res.status(401).send({
+        success: false,
+        message: 'Unauthorized!'
+      });
     }
   }
 };
