481bcf4dd867d30d50869d3d7f1d86cff3dde706
Albert <kovalevskij.albert@gmail.com>
2015-10-11 19:02:03 +0300

    agent controller check access

--- server/api/agent/agent.controller.js
+++ server/api/agent/agent.controller.js
@@ -2,60 +2,118 @@
 
 var _ = require('lodash');
 var Agent = require('./agent.model');
-var uuid = require('node-uuid');
 
 // Get list of agents
-exports.index = function(req, res) {
+exports.index = function (req, res) {
   Agent.scan().exec(function (err, agents) {
-    if(err) { return handleError(res, err); }
+    if (err) {
+      return handleError(res, err);
+    }
+    agents = _.filter(agents, 'isDeleted', false);
     return res.json(200, agents);
   });
 };
 
 // Get a single agent
-exports.show = function(req, res) {
+exports.show = function (req, res) {
   Agent.get(req.params.id, function (err, agent) {
-    if(err) { return handleError(res, err); }
-    if(!agent) { return res.send(404); }
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!agent || !agent.isDeleted) {
+      return res.send(404);
+    }
     return res.json(agent);
   });
 };
 
 // Creates a new agent in the DB.
-exports.create = function(req, res) {
-  req.body.id = uuid.v4();
-  req.body.authId = req.authId;
-  Agent.create(req.body, function(err, agent) {
-    if(err) { return handleError(res, err); }
-    return res.json(201, agent);
-  });
+exports.create = function (req, res) {
+  if (req.body && Object.prototype.toString.call(req.body) === '[object Array]') {
+    var createdItems = [];
+    _.each(req.body, function (item) {
+      checkCanModify(item);
+      restoreDeleted(item);
+      Agent.create(item, function (err, agent) {
+        if (err) {
+          return handleError(res, err);
+        }
+        createdItems.push(agent);
+      });
+      return res.json(201, createdItems);
+    })
+  } else {
+    checkCanModify(req.body);
+    restoreDeleted(req.body);
+    Agent.create(req.body, function (err, agent) {
+      if (err) {
+        return handleError(res, err);
+      }
+      return res.json(201, agent);
+    });
+  }
 };
 
 // Updates an existing agent in the DB.
-exports.update = function(req, res) {
-  if(req.body._id) { delete req.body._id; }
+exports.update = function (req, res) {
+  if (req.body.id) {
+    delete req.body.id;
+  }
   Agent.get(req.params.id, function (err, agent) {
-    if (err) { return handleError(res, err); }
-    if(!agent) { return res.send(404); }
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!agent) {
+      return res.send(404);
+    }
+    checkCanModify(agent);
+    restoreDeleted(agent);
+    if (agent.isDeleted) {
+      delete agent.isDeleted;
+    }
     var updated = _.merge(agent, req.body);
     updated.save(function (err) {
-      if (err) { return handleError(res, err); }
+      if (err) {
+        return handleError(res, err);
+      }
       return res.json(200, agent);
     });
   });
 };
 
 // Deletes a agent from the DB.
-exports.destroy = function(req, res) {
+exports.destroy = function (req, res) {
   Agent.get(req.params.id, function (err, agent) {
-    if(err) { return handleError(res, err); }
-    if(!agent) { return res.send(404); }
-    agent.delete(function(err) {
-      if(err) { return handleError(res, err); }
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!agent || agent.isDeleted) {
+      return res.send(404);
+    }
+    checkCanModify(agent);
+    agent.isDeleted = true;
+    agent.save(function (err) {
+      if (err) {
+        return handleError(res, err);
+      }
       return res.send(204);
     });
   });
 };
+
+function restoreDeleted(agent) {
+  if (agent.isDeleted) {
+    delete agent.isDeleted;
+  }
+}
+
+function checkCanModify(agent) {
+  if (agent.authId !== req.authId) {
+    return res.status(401).send({
+      message: 'Access denied!'
+    });
+  }
+}
 
 function handleError(res, err) {
   return res.send(500, err);
