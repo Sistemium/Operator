eeafd1983c47218e4f8ffe89a248cf28d2bb206e
Albert <kovalevskij.albert@gmail.com>
2015-10-11 20:34:19 +0300

    test

--- server/api/contact/contact.spec.js
+++ server/api/contact/contact.spec.js
@@ -3,13 +3,19 @@
 var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
+var headers = {
+  'Authorization': 'c6dd52d226a821ac9acd45bd92d7a50d@pha'
+};
+var uuid = require('node-uuid');
+var owner = uuid.v4();
+var agent = uuid.v4();
 
 describe('GET /api/contacts', function() {
 
   it('should respond with JSON array', function(done) {
     request(app)
       .get('/api/contacts')
-      .set('Authorization', 'c6dd52d226a821ac9acd45bd92d7a50d@pha')
+      .set(headers)
       .expect(200)
       .expect('Content-Type', /json/)
       .end(function(err, res) {
@@ -17,5 +23,24 @@ describe('GET /api/contacts', function() {
         res.body.should.be.instanceof(Array);
         done();
       });
+  });
+
+  it('should save contact', function(done) {
+    request(app)
+      .post('/api/contacts')
+      .send({
+        owner: owner,
+        agent: agent
+      })
+      .expect(201)
+      .expect('Content-Type', /json/)
+      .end(function (err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        res.body.agent.should.be.exactly(agent);
+        done();
+      })
   });
 });
