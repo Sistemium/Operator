63823b91907e129c984da3a14241e26df8340e45
Albert <kovalevskij.albert@gmail.com>
2015-10-16 16:38:04 +0300

    some cleanup

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -23,9 +23,6 @@ var all = {
   // Server port
   port: process.env.PORT || 9000,
 
-  // Should we populate the DB with sample data?
-  seedDB: false,
-
   // Secret for session, you will want to change this and make it an environment variable
   secrets: {
     session: 'operation-secret'
@@ -34,21 +31,11 @@ var all = {
   // List of user roles
   userRoles: ['guest', 'user', 'admin'],
 
-  // MongoDB connection options
-  mongo: {
-    options: {
-      db: {
-        safe: true
-      }
-    }
-  },
-
   facebook: {
     clientID:     process.env.FACEBOOK_ID || 'id',
     clientSecret: process.env.FACEBOOK_SECRET || 'secret',
     callbackURL:  (process.env.DOMAIN || '') + '/auth/facebook/callback'
   }
-
 };
 
 // Export the config object based on the NODE_ENV
