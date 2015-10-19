0bd3c4376709132a00540a3351a718a46a8d5893
Albert <kovalevskij.albert@gmail.com>
2015-10-19 16:33:56 +0300

    create operation

--- server/api/operation/operation.controller.js
+++ server/api/operation/operation.controller.js
@@ -2,12 +2,116 @@
 
 var _ = require('lodash');
 var Operation = require('./operation.model');
-var uuid = require('node-uuid');
 var Agent = require('../agent/agent.model');
 
 // Get list of operations
 // Get only operations which initiator or executor belongs to user agents
-exports.index = function(req, res) {
+exports.index = function (req, res) {
+  getUserAgents(req, res, function (agentIds) {
+    Operation.scan({
+      isDeleted: false,
+      executor: {'in': agentIds},
+      initiator: {'in': agentIds}
+    }, function (err, operations) {
+      if (err) {
+        return handleError(res, err);
+      }
+      return res.json(200, operations);
+    });
+  });
+};
+
+// Get a single operation
+exports.show = function (req, res) {
+  Operation.get(req.params.id, function (err, operation) {
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!operation) {
+      return res.send(404);
+    }
+    return res.json(operation);
+  });
+};
+
+// Creates a new operation in the DB.
+exports.create = function (req, res) {
+  validate(req, res, function () {
+    Operation.create(req.body, function (err, operation) {
+      if (err) {
+        return handleError(res, err);
+      }
+      return res.json(201, operation);
+    });
+  });
+};
+
+// Updates an existing operation in the DB.
+exports.update = function (req, res) {
+  if (req.body.id) {
+    delete req.body.id;
+  }
+  Operation.get(req.params.id, function (err, operation) {
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!operation) {
+      return res.send(404);
+    }
+    var updated = _.merge(operation, req.body);
+    Operation.update({id: updated.id}, updated, function (err) {
+      if (err) {
+        return handleError(res, err);
+      }
+      return res.json(200, operation);
+    });
+  });
+};
+
+// Deletes a operation from the DB.
+exports.destroy = function (req, res) {
+  Operation.get(req.params.id, function (err, operation) {
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!operation) {
+      return res.send(404);
+    }
+    operation.delete(function (err) {
+      if (err) {
+        return handleError(res, err);
+      }
+      return res.send(204);
+    });
+  });
+};
+
+function validate(req, res, next) {
+  getUserAgents(req, res, function (agentIds) {
+    //check operation initiator is agent's id
+    if (!_.include(agentIds, req.body.initiator)) {
+      return res.send(401, {
+        message: 'Access denied!'
+      });
+    }
+    checkExecutorExist(req.body.executor, res, next);
+  });
+}
+
+function checkExecutorExist(id, res, next) {
+  Agent.get(id, function (err, agent) {
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!agent) {
+      return res.send(404);
+    }
+
+    next();
+  });
+}
+
+function getUserAgents(req, res, next) {
   Agent.scan({authId: req.authId}, function (err, agents) {
     if (err) {
       return handleError(res, err);
@@ -17,57 +121,9 @@ exports.index = function(req, res) {
     }
 
     var agentIds = _.pluck(agents, 'id');
-    Operation.scan({isDeleted: false, executor: {'in': agentIds}, initiator: {'in': agentIds}}, function (err, operations) {
-      if(err) { return handleError(res, err); }
-      return res.json(200, operations);
-    });
+    next(agentIds);
   });
-
-};
-
-// Get a single operation
-exports.show = function(req, res) {
-  Operation.get(req.params.id, function (err, operation) {
-    if(err) { return handleError(res, err); }
-    if(!operation) { return res.send(404); }
-    return res.json(operation);
-  });
-};
-
-// Creates a new operation in the DB.
-exports.create = function(req, res) {
-  req.body.id = uuid.v4();
-  Operation.create(req.body, function(err, operation) {
-    if(err) { return handleError(res, err); }
-    return res.json(201, operation);
-  });
-};
-
-// Updates an existing operation in the DB.
-exports.update = function(req, res) {
-  if(req.body.id) { delete req.body.id; }
-  Operation.get(req.params.id, function (err, operation) {
-    if (err) { return handleError(res, err); }
-    if(!operation) { return res.send(404); }
-    var updated = _.merge(operation, req.body);
-    updated.save(function (err) {
-      if (err) { return handleError(res, err); }
-      return res.json(200, operation);
-    });
-  });
-};
-
-// Deletes a operation from the DB.
-exports.destroy = function(req, res) {
-  Operation.get(req.params.id, function (err, operation) {
-    if(err) { return handleError(res, err); }
-    if(!operation) { return res.send(404); }
-    operation.delete(function(err) {
-      if(err) { return handleError(res, err); }
-      return res.send(204);
-    });
-  });
-};
+}
 
 function handleError(res, err) {
   return res.send(500, err);
--- server/api/operation/operation.spec.js
+++ server/api/operation/operation.spec.js
@@ -3,23 +3,80 @@
 var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
+var sinon = require('sinon');
+var Agent = require('../agent/agent.model');
+var Operation = require('./operation.model');
+var uuid = require('node-uuid');
 var headers = {
   'Authorization': 'c6dd52d226a821ac9acd45bd92d7a50d@pha'
 };
 
-describe('GET /api/operations', function() {
+describe('GET /api/operations', function () {
 
   this.timeout(20000);
 
-  it('should respond with JSON array', function(done) {
+  it('should respond with JSON array', function (done) {
     request(app)
       .get('/api/operations')
       .set(headers)
       .expect(200)
       .expect('Content-Type', /json/)
-      .end(function(err, res) {
+      .end(function (err, res) {
         if (err) return done(err);
         res.body.should.be.instanceof(Array);
+        done();
+      });
+  });
+});
+
+describe('POST /api/operations', function () {
+  var url = '/api/operations';
+  var agentGetStub, agentScanStub, operationCreateStub;
+  beforeEach(function () {
+    agentGetStub = sinon.stub(Agent, 'get');
+    operationCreateStub = sinon.stub(Operation, 'create');
+    agentScanStub = sinon.stub(Agent, 'scan');
+  });
+
+  afterEach(function () {
+    agentGetStub.restore();
+    agentScanStub.restore();
+    operationCreateStub.restore();
+  });
+
+  it.only('should create new operation', function (done) {
+
+    var agents = [{
+      id: uuid.v4(),
+      name: 'test1',
+      authId: 'cbd77f5e-2644-11e5-8000-ffc34d526b60'
+    }, {
+      id: uuid.v4(),
+      name: 'test2',
+      authId: 'cbd77f5e-2644-11e5-8000-ffc34d526b60'
+    }];
+
+    var operation = {
+      id: uuid.v4(),
+      executor: agents[0].id,
+      initiator: agents[1].id
+    };
+
+    agentGetStub.withArgs(operation.executor).yieldsAsync(null, agents[0]);
+    agentScanStub.withArgs({authId: 'cbd77f5e-2644-11e5-8000-ffc34d526b60'}).yieldsAsync(null, agents);
+    operationCreateStub.withArgs(operation).yieldsAsync(null, operation);
+
+    request(app)
+      .post(url)
+      .set(headers)
+      .send(operation)
+      .expect(201)
+      .expect('Content-Type', /json/)
+      .end(function (err) {
+        if (err) return done(err);
+        agentGetStub.calledOnce.should.be.equal(true);
+        agentScanStub.calledOnce.should.be.equal(true);
+        operationCreateStub.calledOnce.should.be.equal(true);
         done();
       });
   });
