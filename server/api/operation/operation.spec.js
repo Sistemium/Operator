4be1bb4f6132fc973059388e48e90b7d09d8099c
Albert <kovalevskij.albert@gmail.com>
2015-10-19 12:19:41 +0300

    operation test

--- server/api/operation/operation.spec.js
+++ server/api/operation/operation.spec.js
@@ -3,13 +3,18 @@
 var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
+var headers = {
+  'Authorization': 'c6dd52d226a821ac9acd45bd92d7a50d@pha'
+};
 
-describe.skip('GET /api/operations', function() {
+describe('GET /api/operations', function() {
+
+  this.timeout(20000);
 
   it('should respond with JSON array', function(done) {
     request(app)
       .get('/api/operations')
-      .set('Authorization', 'c6dd52d226a821ac9acd45bd92d7a50d@pha')
+      .set(headers)
       .expect(200)
       .expect('Content-Type', /json/)
       .end(function(err, res) {
