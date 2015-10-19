fb39a1db351d90f27a09ce3b3cd1a429c0499d53
Albert <kovalevskij.albert@gmail.com>
2015-10-19 12:17:55 +0300

    authorisation for routes

--- server/api/counterAgent/index.js
+++ server/api/counterAgent/index.js
@@ -2,9 +2,10 @@
 
 var express = require('express');
 var controller = require('./counterAgent.controller');
+var auth = require('../../auth/auth.service');
 
 var router = express.Router();
 
-router.get('/:agentId', controller.index);
+router.get('/:agentId', auth.isAuthenticated(), controller.index);
 
 module.exports = router;
