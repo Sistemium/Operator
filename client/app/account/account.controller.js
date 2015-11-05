7bb9d5bafda98a6e4fb464c8ad976c4f22ddc204
Albert <kovalevskij.albert@gmail.com>
2015-11-05 14:30:57 +0200

    currencies for account

--- client/app/account/account.controller.js
+++ client/app/account/account.controller.js
@@ -4,18 +4,30 @@ angular.module('debtApp')
   .factory('Account', function ($resource) {
     return $resource('/api/accounts');
   })
-  .controller('AccountCtrl', function ($stateParams, Account) {
+  .factory('Currency', function ($resource) {
+    return $resource('/api/currencies')
+  })
+  .controller('AccountCtrl', function ($stateParams, Account, Currency) {
     var me = this;
     me.accounts = [];
+    me.currencies = [];
     var agentId = $stateParams.agentId;
     var accountsPromise = Account.query();
+    var currenciesPromise = Currency.query();
 
     angular.extend(me, {
       getData: function () {
+        if (currenciesPromise.hasOwnProperty('$promise')) {
+          currenciesPromise.$promise.then(function (res) {
+            me.currencies = res;
+          });
+        } else {
+          me.currencies = currenciesPromise;
+        }
         if (accountsPromise.hasOwnProperty('$promise')) {
           accountsPromise.$promise.then(function (res) {
             me.accounts = res;
-          })
+          });
         } else {
           me.accounts = accountsPromise;
         }
