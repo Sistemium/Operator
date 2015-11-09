98d521405241625d11519fa9d34f8a1dff8b7a29
Albert <kovalevskij.albert@gmail.com>
2015-11-09 12:51:21 +0200

    operation view

--- client/app/operation/operation.controller.js
+++ client/app/operation/operation.controller.js
@@ -1,14 +1,16 @@
 'use strict';
 
 angular.module('debtApp')
-  .controller('OperationCtrl', ['$stateParams', 'CounterAgent', 'Account', 'Operation',
-    function ($stateParams, CounterAgent, Account, Operation) {
+  .controller('OperationCtrl', ['$stateParams', 'CounterAgent', 'Account', 'Operation', 'Currency',
+    function ($stateParams, CounterAgent, Account, Operation, Currency) {
       var me = this;
       me.counterAgents = [];
       var agentId = $stateParams.agent;
 
       me.init = function () {
+        me.operationsPromise = Operation.query();
         me.counterAgentsPromise = CounterAgent.query({agent: agentId});
+        me.currenciesPromise = Currency.query();
 
         if (me.counterAgentsPromise.hasOwnProperty('$promise')) {
           me.counterAgentsPromise.$promise.then(function (res) {
@@ -18,7 +20,25 @@ angular.module('debtApp')
             me.counterAgents = res;
           });
         } else {
-          me.counterAgents = counterAgentsPromise;
+          me.counterAgents = me.counterAgentsPromise;
+        }
+
+        if (me.currenciesPromise.hasOwnProperty('$promise')) {
+          me.currenciesPromise.$promise.then(function (res) {
+            me.currencies = res;
+            me.currency = res[0];
+          });
+        } else {
+          me.currencies = me.currenciesPromise;
+          me.currency = me.currenciesPromise[0];
+        }
+
+        if (me.operationsPromise.hasOwnProperty('$promise')) {
+          me.operationsPromise.$promise.then(function (res) {
+            me.operations = res;
+          });
+        } else {
+          me.operations = me.operationsPromise;
         }
       };
 
@@ -26,17 +46,24 @@ angular.module('debtApp')
         var operation = {
           id: uuid.v4(),
           sumTotal: me.sumTotal,
-          currency: me.chosenAccount.currency.id,
-          initiator: accountId,
-          executor: me.chosenAccount.id,
-          remindDuration: 'date until'
+          currency: me.currency,
+          initiator: agentId,
+          executor: me.chosenContact.id,
+          remindDuration: Date.now() + 24*60*60*1000
         };
 
-        Operation.save(operation).$promise.then(function () {
+        Operation.save(operation).$promise.then(function (res) {
           alert('Операция сохранена');
+          me.showOperationCreateForm = false;
+          me.operations.push(res);
         }, function () {
           alert('Неудача');
-        })
+        });
+      };
+
+      me.createOperation = function (counterAgent) {
+        me.showOperationCreateForm = true;
+        me.chosenContact = counterAgent;
       };
 
       me.init();
