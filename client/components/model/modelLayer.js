2facd1be183354dbab1db455a2e042a8167dd5a5
Albert <kovalevskij.albert@gmail.com>
2015-12-03 16:48:01 +0200

    relations

--- client/components/model/modelLayer.js
+++ client/components/model/modelLayer.js
@@ -8,15 +8,29 @@
           basePath: '/api'
         });
       }])
-    .run(['$rootScope', 'Agent', 'Currency', 'Invite', 'Auth', 'messageBus',
-      function ($rootScope, Agent, Currency, Invite, Auth, messageBus) {
+    .run(['$rootScope', 'Agent', 'Currency', 'Invite', 'Operation', 'Account', 'Auth', 'messageBus',
+      function ($rootScope, Agent, Currency, Invite, Operation, Account, Auth, messageBus) {
         Auth.isLoggedInAsync(function (isLoggedIn) {
           if (isLoggedIn) {
             //register socket events
             messageBus.initSocket();
-            Agent.findAll();
-            Currency.findAll();
-            Invite.findAll();
+
+            // initially fetch data and inject in stores
+            Agent.findAll().then(function (res) {
+              console.log(res);
+            });
+            Currency.findAll().then(function (res) {
+              console.log(res);
+            });
+            Invite.findAll().then(function (res) {
+              console.log(res);
+            });
+            Operation.findAll().then(function (res) {
+              console.log(res);
+            });
+            Account.findAll().then(function (res) {
+              console.log(res);
+            })
           }
         });
       }])
@@ -27,29 +41,130 @@
     }])
     .service('Agent', ['DS', function (DS) {
       return DS.defineResource({
-        name: 'agents'
+        name: 'agents',
+        relations: {
+          hasMany: {
+            invites: [
+              {
+                localField: 'ownerInvites',
+                foreignKey: 'owner'
+              }, {
+                localField: 'acceptorInvites',
+                foreignKey: 'acceptor'
+              }
+            ],
+            operations: [
+              {
+                localField: 'lenderOperations',
+                foreignKey: 'lender'
+              },
+              {
+                localField: 'debtorOperations',
+                foreignKey: 'debtor'
+              }
+            ]
+          }
+        }
       });
     }])
     .service('Invite', ['DS', function (DS) {
       return DS.defineResource({
-        name: 'invites'
+        name: 'invites',
+        relations: {
+          belongsTo: {
+            agents: [
+              {
+                localField: 'ownerAgent',
+                localKey: 'owner'
+              },
+              {
+                localField: 'acceptorAgent',
+                localKey: 'acceptor'
+              }
+            ]
+          }
+        }
       });
     }])
     .service('AgentInvite', ['DS', function (DS) {
       return DS.defineResource({
         name: 'agentInvites',
-        endpoint: '/invites/agentInvites'
+        endpoint: '/invites/agentInvites',
+        relations: {
+          belongsTo: {
+            agents: [
+              {
+                localField: 'ownerAgent',
+                localKey: 'owner'
+              },
+              {
+                localField: 'acceptorAgent',
+                localKey: 'acceptor'
+              }
+            ]
+
+          }
+        }
       });
     }])
     .service('AgentOperation', ['DS', function (DS) {
       return DS.defineResource({
         name: 'agentOperations',
-        endpoint: '/operations/agentOperations'
+        endpoint: '/operations/agentOperations',
+        relations: {
+          belongsTo: {
+            agents: [
+              {
+                localField: 'lenderAgent',
+                localKey: 'lender'
+              },
+              {
+                localField: 'debtorAgent',
+                localKey: 'debtor'
+              }
+            ]
+          },
+          hasOne: {
+            currencies: {
+              localField: 'currencyEntity',
+              localKey: 'currency'
+            }
+          }
+        }
       });
     }])
     .service('Operation', ['DS', function (DS) {
       return DS.defineResource({
-        name: 'operations'
+        name: 'operations',
+        relations: {
+          belongsTo: {
+            agents: [
+              {
+                localField: 'lenderAgent',
+                localKey: 'lender'
+              },
+              {
+                localField: 'debtorAgent',
+                localKey: 'debtor'
+              }
+            ],
+            accounts: [
+              {
+                localField: 'lenderAccountEntity',
+                localKey: 'lenderAccount'
+              }, {
+                localField: 'debtorAccountEntity',
+                localKey: 'debtorAccount'
+              }
+            ]
+          },
+          hasOne: {
+            currencies: {
+              localField: 'currencyEntity',
+              localKey: 'currency'
+            }
+          }
+        }
       });
     }])
     .service('CounterAgent', ['DS', function (DS) {
@@ -59,7 +174,20 @@
     }])
     .service('Account', ['DS', function (DS) {
       return DS.defineResource({
-        name: 'accounts'
+        name: 'accounts',
+        relations: {
+          hasMany: {
+            operations: [
+              {
+                localField: 'debtorAccountOperations',
+                foreignKey: 'debtorAccount'
+              }, {
+                localField: 'lenderAccountOperations',
+                foreignKey: 'lenderAccount'
+              }
+            ]
+          }
+        }
       })
     }]);
 })();
