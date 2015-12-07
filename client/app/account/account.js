7d84fedf674161632530508c1c1d480071517c70
Albert <kovalevskij.albert@gmail.com>
2015-12-07 17:35:23 +0200

    account operations

--- client/app/account/account.js
+++ client/app/account/account.js
@@ -10,7 +10,21 @@
           controller: 'AccountCtrl',
           controllerAs: 'ctrl',
           authorize: true
-        });
+        })
+      .state('accountOperations', {
+        url: '/:agent/:account/operations',
+        templateUrl: 'app/account/accountOperations.html',
+        controller: 'AccountOperationsCtrl',
+        controllerAs: 'ctrl',
+        authorize: true,
+        resolve:  {
+          accountOperations: function ($stateParams, Account) {
+            return Account.find($stateParams.account).then(function (res) {
+              return res.debtorAccountOperations.concat(res.lenderAccountOperations);
+            });
+          }
+        }
+      })
     }])
   ;
 })();
