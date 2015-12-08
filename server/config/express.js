8823cccdfda3d7c61ab1b9dfa28fe965fae6fbc7
Albert <kovalevskij.albert@gmail.com>
2015-12-08 17:30:47 +0200

    remove unused libs

--- server/config/express.js
+++ server/config/express.js
@@ -10,7 +10,6 @@ var morgan = require('morgan');
 var compression = require('compression');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-var cookieParser = require('cookie-parser');
 var errorHandler = require('errorhandler');
 var path = require('path');
 var config = require('./environment');
@@ -25,7 +24,6 @@ module.exports = function (app) {
   app.use(bodyParser.urlencoded({extended: false}));
   app.use(bodyParser.json());
   app.use(methodOverride());
-  app.use(cookieParser());
   app.set('redisdb', config.redis[env]);
 
   if ('production' === env) {
