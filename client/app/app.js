1d379ac31f49232d6c573fb4ef1aed5209fef65b
Albert <kovalevskij.albert@gmail.com>
2016-02-15 09:04:49 +0200

    models

--- client/app/app.js
+++ client/app/app.js
@@ -1,13 +1,23 @@
 'use strict';
 
 angular.module('debtApp', [
-  'debtApp.constants',
-  'ngResource',
-  'ngSanitize',
-  'ui.router',
-  'ui.bootstrap'
-])
-  .config(function($urlRouterProvider) {
+    'debtApp.constants',
+    'ngResource',
+    'js-data',
+    'ngSanitize',
+    'ui.router',
+    'ui.bootstrap'
+  ])
+  .config(function ($urlRouterProvider) {
     $urlRouterProvider
       .otherwise('/');
-  });
+  })
+
+  .config(function (DSProvider, DSHttpAdapterProvider) {
+    angular.extend(DSProvider.defaults, {});
+    angular.extend(DSHttpAdapterProvider.defaults, {
+      basePath: 'http://localhost:1337'
+    });
+  })
+;
+
